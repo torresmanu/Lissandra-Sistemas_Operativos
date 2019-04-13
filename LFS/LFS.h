8e417aa17587407cadfaedf4ad2c014d00ea7907
Juan Martin Torres <juanmar.torres@hotmail.com>
2019-04-13 14:30:11 -0300

    Se agrega el parseo y utils, resta agregar validaciones en el parseo

--- LFS/LFS.h
+++ LFS/LFS.h
@@ -15,11 +15,33 @@
 #include<commons/config.h>
 #include<readline/readline.h>
 
+typedef enum
+{
+	OK,
+	SALIR,
+	MENSAJE_MAL_FORMATEADO,
+	ERROR
+}estado;
+
+typedef struct
+{
+	estado resultado;
+	char* mensaje;
+} resultado;
+
 t_log* g_logger;
 t_config* g_config;
 
 
 void iniciar_programa(void);
+resultado parsear_mensaje(char *);
+resultado select_acc(char *,int);
+resultado insert(char*,int,char*,long);
+resultado create(char*,char*,int,int);
+resultado describe(char*);
+resultado drop(char*);
+resultado journal();
+
 void terminar_programa(void);
 
 #endif /* LFS_H_ */
