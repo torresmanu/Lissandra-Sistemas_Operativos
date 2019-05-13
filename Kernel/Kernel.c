83de5f79ace0dea0cde719368e5cf681e0669a91
alevinas <alefv.97@gmail.com>
2019-05-13 11:21:45 -0300

    Arranco estructuras de procesos y la request RUN

--- Kernel/Kernel.c
+++ Kernel/Kernel.c
@@ -15,7 +15,7 @@ int main(void) {
 	iniciar_programa();
 	gestionarConexion();
 	terminar_programa();
-return 0;
+	return 0;
 }
 
 void iniciar_programa(void)
@@ -29,7 +29,6 @@ void iniciar_programa(void)
 	log_info(g_logger,"Configuraciones inicializadas");
 
 }
-
 
 void terminar_programa()
 {
@@ -49,5 +48,49 @@ void gestionarConexion()
 	while(i){
 		i=enviar_mensaje(socketServer);	//Con "exit" i=0 y salgo
 	}
+}
+
+// Propias de Kernel
+// Estados -> Colas
+void iniciarEstados(){
+	NEW = queue_create();
+	READY = queue_create();
+	EXEC = queue_create();
+	EXIT = queue_create();
+}
+
+void finalizarEstados(){
+	queue_clean_and_destroy_elements(NEW,liberarEstado);
+	queue_clean_and_destroy_elements(READY,liberarEstado);
+	queue_clean_and_destroy_elements(EXEC,liberarEstado);
+	queue_clean_and_destroy_elements(EXIT,liberarEstado);
+}
+
+void liberarEstado(void* elem){
+	// Falta inicializar una estructura que contenga el proceso
+	// y hacerle un free. Pero me falta pensar que estructura lo contiene.
+	// free(t_proceso*); por asi decirlo
+}
+
+void agregarScriptAEstado(){}
+void moverScriptDeEstado(){}
+void finalizarScript(){} // Debe hacer un free y sacarlo de la cola
+
+// Leer LQL
+
+void leerArchivoLQL(){		// Itera ejecutando leer ScriptLQL
 
 }
+
+void leerScriptLQL(){}
+
+void RUN(FILE* path){
+	FILE* lql = fopen(path,"r+b");
+	// t_proceso* proc;
+	while(!feof(lql)){
+		//
+	}
+}
+
+/* Necesito parsear_mensaje() para ir haciendolo con cada script. Asi no uso otra funcion igual */
+
--- Kernel/Kernel.h
+++ Kernel/Kernel.h
@@ -8,20 +8,35 @@
 #ifndef KERNEL_H_
 #define KERNEL_H_
 
-#include<commons/log.h>
-#include<commons/config.h>
-#include<commons/sockets.h>
+#include <commons/collections/queue.h>
+#include <commons/log.h>
+#include <commons/config.h>
+#include <commons/sockets.h>
 
 t_log* g_logger;
 t_config* g_config;
-
 char* IP;
 char* PUERTO;
+
+// SCRIPT (Con tratamiento de procesos, por eso usaremos colas como estados)
+
+t_queue* NEW;
+t_queue* READY;
+t_queue* EXEC;
+t_queue* EXIT;
+void iniciarEstados();
+void finalizarEstados();
+
+
 
 void iniciar_programa(void);
 void terminar_programa(void);
 int enviar_mensaje(int socket_cliente);
 int iniciarCliente();
 void gestionarConexion();
+void iniciarEstados();
+
+
+
 
 #endif /* KERNEL_H_ */
