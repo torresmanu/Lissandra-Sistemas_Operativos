0dae276b3bce0dbc588a37d2d1a3bd66e2164543
Juan Martin Torres <juanmar.torres@hotmail.com>
2019-06-02 12:37:57 -0300

    Se afrega parse

--- LFS/registro.c
+++ LFS/registro.c
@@ -8,9 +8,9 @@
 #include "registro.h"
 
 void parseRegistro(char* linea,registro* regAux){
-	char * str_timestamp = strsep(&linea,";");
-	char * str_key = strsep(&linea,";");
-	regAux->value = strsep(&linea,"\n");
-	regAux->key = atoi(str_key);
-	regAux->timestamp = atol (str_timestamp);
+
+	regAux->timestamp = atol (strsep(&linea,";"));
+	regAux->key = atoi(strsep(&linea,";"));
+	strcpy(regAux->value, strsep(&linea,"\n"));
+
 }
