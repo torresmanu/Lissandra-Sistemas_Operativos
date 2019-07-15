fe10222acbf6d63c65b0487e0d8d3f16e4fdcd0f
alevinas <alefv.97@gmail.com>
2019-07-14 23:11:38 -0300

    Metricas en milisegundos

--- Kernel/Kernel.c
+++ Kernel/Kernel.c
@@ -25,7 +25,7 @@ int main(void) {
 	pthread_create(&describeGlobal,NULL,(void*)realizarDescribeGlobal,NULL);
 	pthread_create(&gossipingAutomatico,NULL,(void*)realizarGossipingAutomatico,NULL);
 	pthread_create(&monitoreador,NULL,(void*)controlConfig,NULL);
-	pthread_create(&metricas,NULL,(void*)realizarMetrics,NULL);
+	//pthread_create(&metricas,NULL,(void*)realizarMetrics,NULL);
 
 	leerConsola();											/// ACA COMIENZA A ITERAR Y LEER DE STDIN /////
 
@@ -33,8 +33,8 @@ int main(void) {
 	{
 		pthread_join(plp,NULL);
 		pthread_join(describeGlobal,NULL);
-		pthread_join(metricas,NULL);
-		pthread_join(monitoreador,NULL);
+		//pthread_join(metricas,NULL);
+		//pthread_join(monitoreador,NULL);
 
 		for(int i=0; i<nivelMultiprocesamiento; i++)
 		{
@@ -242,6 +242,7 @@ void ejecutador(){ // ACTUA COMO ESTADO EXEC
 			}
 			else if(e.resultado == ERROR){
 				log_error(g_logger, "Error en request n°: %d", s->pc);
+				log_error(g_logger, "Request abortada");
 				mandarAexit(s);
 				break;
 			}
@@ -249,6 +250,7 @@ void ejecutador(){ // ACTUA COMO ESTADO EXEC
 			if (terminoScript(s)) {
 				printf("\n");
 				log_info(g_logger, "Termino script");
+				printf("\n");
 				mandarAexit(s);
 				break;
 			}
@@ -286,11 +288,8 @@ void realizarDescribeGlobal()
 {
 	while(1)
 	{
-		sem_wait(&sDescribe);
-		//printf("La metadata refresh es: %d\n", metadataRefresh);
 		describe(NULL);
 		usleep(metadataRefresh*1000); // Lo paso a ms
-		sem_post(&sDescribe);
 	}
 }
 
@@ -528,7 +527,7 @@ void mostrarMetrics(Criterio* crit)
 	}
 	else
 	{
-		log_info(g_logger, "Read Latency / 30s %s: %ld ms", mostrarConsistencia(crit->tipo), crit->timeTotalReads / crit->amountReads);
+		log_info(g_logger, "Read Latency / 30s %s: %" PRIu64 "ms", mostrarConsistencia(crit->tipo), crit->timeTotalReads / crit->amountReads);
 
 	}
 	if(crit->amountWrites == 0)
@@ -537,7 +536,7 @@ void mostrarMetrics(Criterio* crit)
 	}
 	else
 	{
-		log_info(g_logger, "Write Latency / 30s %s: %ld ms", mostrarConsistencia(crit->tipo), crit->timeTotalWrites / crit->amountWrites);
+		log_info(g_logger, "Write Latency / 30s %s: %" PRIu64 "ms", mostrarConsistencia(crit->tipo), crit->timeTotalWrites / crit->amountWrites);
 	}
 
 	// Dejo en cero todooo
